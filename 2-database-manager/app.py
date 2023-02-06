fca90ea8a90989dd844081b35d1a684cd4d09238
viviane <vivianedasilvasouza2020@gmail.com>
2023-02-06 20:35:20 -0300

    feat:consultando usuarios

--- 2-database-manager/app.py
+++ 2-database-manager/app.py
@@ -1,15 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
 db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(84), nullable=False)
+    email = db.Column(db.String(84), nullable=False, unique=True, index=True)
+    password = db.Column(db.String(255), nullable=False)
+
+    def __str__(self):
+        return self.name
 
 
 @app.route("/")
 def index():
-    return "página em branco"
+    users = User.query.all()
+    return render_template("user.html", users=users)
 
 
 if __name__ == "__main__":
