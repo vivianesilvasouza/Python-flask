0366052130fd3e88fde8edb097306dbf3d36107b
viviane <vivianedasilvasouza2020@gmail.com>
2023-02-07 15:16:38 -0300

    feat: relacionamento d tabela

--- 2-database-manager/app.py
+++ 2-database-manager/app.py
@@ -14,6 +14,17 @@ class User(db.Model):
     name = db.Column(db.String(84), nullable=False)
     email = db.Column(db.String(84), nullable=False, unique=True, index=True)
     password = db.Column(db.String(255), nullable=False)
+    profile = db.relationship("Profile", backref="user", uselist=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Profile(db.Model):
+    __tablename__ = "profiles"
+    id = db.Column(db.Integer, primary_key=True)
+    photo = db.Column(db.Unicode(124), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
 
     def __str__(self):
         return self.name
